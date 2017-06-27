602740eb4932f87dda18d731a9044380e0d10807
Carlos Espacios <carlspace@hotmail.com>
2017-06-27 21:07:45 +0100

    cambios Dummy -> inicial

--- DummyApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/app/Mediator.java
+++ DummyApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/app/Mediator.java
@@ -1,7 +1,7 @@
 package es.ulpgc.eite.clean.mvp.sample.app;
 
-import es.ulpgc.eite.clean.mvp.sample.dummy.Dummy;
+import es.ulpgc.eite.clean.mvp.sample.inicial.Inicial;
 
 public interface Mediator {
-  void startingDummyScreen(Dummy.ToDummy presenter);
+  void startingInicialScreen(Inicial.ToInicial presenter);
 }

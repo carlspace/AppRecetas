726bfd7548ab00047f00a6a7e19d1ad618d1e592
Carlos Espacios <carlspace@hotmail.com>
2017-06-28 20:16:32 +0100

    inicio base de datos

--- DummyApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/database/Plato.java
+++ DummyApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/database/Plato.java
@@ -1,8 +1,19 @@
 package es.ulpgc.eite.clean.mvp.sample.database;
+
 import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
 /**
  * Created by Carlos Espacios on 28/06/2017.
  */
 
 public class Plato extends RealmObject {
+
+    @PrimaryKey
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private int idTipoDeComida;
+    private String imagen;
+    private Boolean isInAssetsPlato;
 }

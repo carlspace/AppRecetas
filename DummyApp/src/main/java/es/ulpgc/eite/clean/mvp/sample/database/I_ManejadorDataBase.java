a96f0a706a7f5d1153962d228e4c9874973824b8
Carlos Espacios <carlspace@hotmail.com>
2017-06-29 17:56:07 +0100

    inicio del manerjador de la base de datos

---
+++ DummyApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/database/I_ManejadorDataBase.java
@@ -0,0 +1,23 @@
+package es.ulpgc.eite.clean.mvp.sample.database;
+
+/**
+ * Created by Carlos Espacios on 29/06/2017.
+ */
+
+public interface I_ManejadorDataBase {
+
+
+
+
+    //TipoDeComida
+    String get
+    //platos
+
+
+
+
+
+
+    //plato
+
+}

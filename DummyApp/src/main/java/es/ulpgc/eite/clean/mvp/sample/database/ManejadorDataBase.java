36b70127f3ba03f532ab7d4c355fe0f36e8f2eae
Carlos Espacios <carlspace@hotmail.com>
2017-07-01 13:05:34 +0100

    dataBase muy real

--- DummyApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/database/ManejadorDataBase.java
+++ DummyApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/database/ManejadorDataBase.java
@@ -120,32 +120,68 @@ public class ManejadorDataBase implements I_ManejadorDataBase {
             addTipoDeComida("Ensaladas");
                 int idTipoDeComida=1;
                         //plato Cogollos de Tudela con anchoas y queso (id=1)
-                        String nombrePlato = "";
-                        String receta = "";
-                        addPlato(nombrePlato, receta, idTipoDeComida, "", "",true);
-                    //plato Cogollos de Tudela con anchoas y queso (id=1)
-                 nombrePlato = "";
-             receta = "";
-            addPlato(nombrePlato, receta, idTipoDeComida, "", "",true);
-            //plato Cogollos de Tudela con anchoas y queso (id=1)
-             nombrePlato = "";
-             receta = "";
-            addPlato(nombrePlato, receta, idTipoDeComida, "", "",true);
-            //plato Cogollos de Tudela con anchoas y queso (id=1)
-             nombrePlato = "";
-             receta = "";
-            addPlato(nombrePlato, receta, idTipoDeComida, "", "",true);
-            //plato Cogollos de Tudela con anchoas y queso (id=1)
-             nombrePlato = "";
-             receta = "";
-            addPlato(nombrePlato, receta, idTipoDeComida, "", "",true);
-            //plato Cogollos de Tudela con anchoas y queso (id=1)
-             nombrePlato = "";
-             receta = "";
-            addPlato(nombrePlato, receta, idTipoDeComida, "", "",true);
+                        String nombrePlato = "Cogollos de Tudela con anchoas y queso";
+
+                        String receta = "El primer paso para empezar a preparar estos cogollos de Tudela con anchoas y queso es poner los cogollos de lechuga en remojo en agua bien fría. Los dejaremos 5 minutos.\n" +
+                                "Por otro lado, vamos picando los tomates y las cebollas.\n" +
+                                "A continuación, ponemos las verduras picadas en un bol y las aliñamos con un poco de aceite y vinagre al gusto.\n" +
+                                "\n" +
+                                "Entonces lo removemos todo y les echamos un poco de sal y pimienta a nuestro gusto también.\n" +
+                                "A continuación, sacamos los cogollos del agua y escurrimos bien. Los cortamos en rodajas.\n" +
+                                "Seguidamente los ponemos en una fuente, cortamos el queso de cabra en rodajas y los vamos poniendo encima de cada cogollo.\n" +
+                                "\n" +
+                                "Encima de los cogollos de lechuga con queso ponemos las anchoas.\n" +
+                                "Posteriormente picamos las aceitunas y hacemos tiras de los pimientos del piquillo, y lo vamos colocando encima de los cogollos de Tudela con anchoas y queso.\n" +
+                                "\n" +
+                                "En el bol donde tenemos todo picadito y aliñado ponemos un poco de perejil o cilantro. Lo removemos bien. Esta será la vinagreta para los cogollos con anchoas\n" +
+                                "Después aliñaremos nuestros cogollos de lechuga con queso y anchoas con esta vinagreta. ¡Y ya estarán listos!\n";
+                        addPlato(nombrePlato, receta, idTipoDeComida, "", "https://www.youtube.com/watch?v=gr3LiqwIVx4",true);
+
+
+                        //Pimientos asados con bonito(id=2)
+                        nombrePlato = "Pimientos asados con bonito";
+                        receta = "Comenzaremos a hacer esta receta de pimientos asados con ventresca de bonito asando los pimientos.\n" +
+                                "Pues bien, cuando los pimientos estén asados y atemperados, y después de pelarlos, los cortamos en tiras" +
+                                " y los colocamos sobre una fuente. Añadimos un poco de sal al gusto y removemos.\n" +
+                                "A continuación, colocamos los lomos de bonito del norte por encima y regamos con un poco del aceite del mismo" +
+                                " o bote o si preferimos con un poco de aceite de oliva virgen extra\n";
+
+                        addPlato(nombrePlato, receta, idTipoDeComida, "", "https://www.youtube.com/watch?v=gh5sdgbvpJU",true);
+
+                        //plato Ensalada de buey de mar (id=3)
+                        nombrePlato = "Ensalada de buey de mar";
+                        receta = "Empezaremos por hervir los bueyes de mar en una olla grande con abundante agua salada durante 15 minutos. Escurrirlos, " +
+                                "abrirlos y separar el interior, presionando en la parte inferior.\n" +
+                                "Sacar la carne con la ayuda de una cuchara y picarla finamente con un cuchillo afilado. Reservar los caparazones y " +
+                                "el jugo que haya quedado al picar la carne. Esta forma es la más práctica de cocinar buey de mar.\n" +
+                                "Por otro lado, nos ponemos a preparar la verdura para la ensalada de cangrejo. Entonces, pelar y picar la cebolla, " +
+                                "lavar los cogollos de lechuga y dejarlos escurrir bien.\n" +
+                                "Poner en un bol, la cebolla, la lechuga, el apio y los huevos duros todo picado muy pequeño.\n" +
+                                "\n" +
+                                "A continuación, añadir la carne de buey y mezclar bien. Poner la mayonesa en un cuenco y añadir el concentrado de tomate, el jugo reservado y el brandy.\n" +
+                                "Mezclar bien todo y sazonar al gusto si lo consideras necesario.\n" +
+                                "Cuando ya tengamos la mezcla lista, solo nos queda rellenar los caparazones con nuestra ensalada de buey de mar y servir.\n";
+
+                        addPlato(nombrePlato, receta, idTipoDeComida, "", "https://www.youtube.com/watch?v=81Jo4YGuRCY",true);
+
 
             addTipoDeComida("Sopa");
             idTipoDeComida=2;
+
+
+
+
+
+
+
+            addTipoDeComida("Carnes");
+            idTipoDeComida=3;
+
+
+
+
+            addTipoDeComida("Pescado");
+            idTipoDeComida=4;
 
         }
         }

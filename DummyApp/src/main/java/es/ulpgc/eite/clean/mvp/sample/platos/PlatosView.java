f38f1c54195eb9892ef0c47668555bc36224159f
Carlos Espacios <carlspace@hotmail.com>
2017-07-01 11:54:30 +0100

    activity_platos

--- DummyApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/platos/PlatosView.java
+++ DummyApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/platos/PlatosView.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;

028f6ac95528f59eaeaab5bf8b406159b5b16d40
Erik Martrus <lemurm20@gmail.com>
2017-07-04 18:45:12 +0100

    Creado paquete AddPlato con su modelo, vista, presentador y su interfaz

---
+++ DummyApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/AddPlato/AddPlatoModel.java
@@ -0,0 +1,10 @@
+package es.ulpgc.eite.clean.mvp.sample.AddPlato;
+
+import AddPlato;
+
+/**
+ * Created by Erik on 04/07/2017.
+ */
+
+public class AddPlatoModel implements AddPlato {
+}
---
+++ DummyApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/AddPlato/AddPlatoView.java
@@ -0,0 +1,8 @@
+package es.ulpgc.eite.clean.mvp.sample.AddPlato;
+
+/**
+ * Created by Erik on 04/07/2017.
+ */
+
+public class AddPlatoView {
+}

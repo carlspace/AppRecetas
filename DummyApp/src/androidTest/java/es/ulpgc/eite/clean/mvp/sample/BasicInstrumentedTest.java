0e7420dd7dd89f238666b572fb20d0c28dd5ec93
Carlos Espacios <carlspace@hotmail.com>
2017-07-11 14:07:18 +0100

    tests toolbar con nombre de plato segun categoria

--- DummyApp/src/androidTest/java/es/ulpgc/eite/clean/mvp/sample/BasicInstrumentedTest.java
+++ DummyApp/src/androidTest/java/es/ulpgc/eite/clean/mvp/sample/BasicInstrumentedTest.java
@@ -4,22 +4,30 @@ import android.content.pm.ActivityInfo;
 import android.os.SystemClock;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import es.ulpgc.eite.clean.mvp.sample.database.Plato;
+import es.ulpgc.eite.clean.mvp.sample.database.TipoComida;
 import es.ulpgc.eite.clean.mvp.sample.inicial.InicialView;
 import io.realm.Realm;
 import io.realm.RealmResults;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -59,9 +67,56 @@ public class BasicInstrumentedTest {
         onView(withId(R.id.textoBtnPescado)).check(matches(withText("Pescados")));
     }
 
+    @Test
+    public void testDisplaylistaPlatosEnsalada() {
+        onView(withId(R.id.btnEnsalada)).perform(click());
+        //comprobar el titulo de Toolbar
+        onView(allOf(isAssignableFrom(TextView.class), withParent(isAssignableFrom(Toolbar.class))))
+                .check(matches(withText("Ensaladas")));
+        onView(allOf(withId(R.id.listaPlatos),isDisplayed()));
+        onData(anything())
+                .inAdapterView(withId(R.id.listaPlatos))
+                .atPosition(0)
+                .check(matches(withText("Cogollos de Tudela con anchoas y queso")));
+    }
+    @Test
+    public void testDisplaylistaPlatosSopa() {
+        onView(withId(R.id.btnSopa)).perform(click());
+        //comprobar el titulo de Toolbar
+        onView(allOf(isAssignableFrom(TextView.class), withParent(isAssignableFrom(Toolbar.class))))
+                .check(matches(withText("Sopas")));
+        onView(allOf(withId(R.id.listaPlatos),isDisplayed()));
+        onData(anything())
+                .inAdapterView(withId(R.id.listaPlatos))
+                .atPosition(0)
+                .check(matches(withText("Sopa castellana")));
+    }
 
+    @Test
+    public void testDisplaylistaPlatosCarne() {
+        onView(withId(R.id.btnCarne)).perform(click());
+        //comprobar el titulo de Toolbar
+        onView(allOf(isAssignableFrom(TextView.class), withParent(isAssignableFrom(Toolbar.class))))
+                .check(matches(withText("Carnes")));
+        onView(allOf(withId(R.id.listaPlatos),isDisplayed()));
+        onData(anything())
+                .inAdapterView(withId(R.id.listaPlatos))
+                .atPosition(0)
+                .check(matches(withText("Carne guisada con papas")));
+    }
 
-
+    @Test
+    public void testDisplaylistaPlatosPescado() {
+        onView(withId(R.id.btnPescado)).perform(click());
+        //comprobar el titulo de Toolbar
+        onView(allOf(isAssignableFrom(TextView.class), withParent(isAssignableFrom(Toolbar.class))))
+                .check(matches(withText("Pescados")));
+        onView(allOf(withId(R.id.listaPlatos),isDisplayed()));
+        onData(anything())
+                .inAdapterView(withId(R.id.listaPlatos))
+                .atPosition(0)
+                .check(matches(withText("Salmon encebollado")));
+    }
 
 
     ////////Test de la base de datos (realm)
@@ -82,7 +137,13 @@ public class BasicInstrumentedTest {
         assertEquals(ids[1],array[1]);
         assertEquals(ids[2],array[2]);
     }
-
+    @Test
+    public void testRealmGetNombreTipoComida(){
+        Realm realm = Realm.getDefaultInstance();
+        RealmResults<TipoComida> result= realm.where(TipoComida.class).equalTo("id",1).findAll();
+        String nombre = result.get(0).getNombreTipoComida();
+        assertEquals(nombre,"Ensaladas");
+    }
 
 
 

c84fa3a5c372e9340b6222661c01c8f1a5744216
Carlos Espacios <carlspace@hotmail.com>
2017-10-02 16:57:24 +0100

    inicio youtube

---
+++ DummyApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/youtube/ConfigYoutube.java
@@ -0,0 +1,14 @@
+package es.ulpgc.eite.clean.mvp.sample.youtube;
+
+/**
+ * Created by Carlos Espacios on 26/09/2017.
+ */
+
+public class ConfigYoutube {
+
+    // Google Console APIs developer clave
+    public static final String DEVELOPER_KEY = "AIzaSyCJ4yOn8M9R-nAM5Lb9GSL5KrbVqo_AL00";
+
+    // YouTube video id
+    public static final String YOUTUBE_VIDEO_CODE = "K4wv_dwmiwQ";
+}
